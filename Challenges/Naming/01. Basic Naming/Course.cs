cdf6bb8e7acc9818136561c78e2ccd2b8488c30b
Nikola Luburic <luburic91@gmail.com>
2021-03-31 14:40:24 +0200

    feat: Adds starting challenges for Naming topic.

---
+++ Challenges/Naming/01. Basic Naming/Course.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Naming._01._Basic_Naming
+{
+    /// <summary>
+    /// 1. Identify and rename all the identifiers with poor names (excluding the namespace).
+    /// </summary>
+    class CourseService
+    {
+        public void Add(Course nc, Student s)
+        {
+            int i = 0;
+            //Counts number of active courses
+            foreach (var c in s.Courses)
+            {
+                if (c.Status.Equals("enrolled") || c.Status.Equals("current")) i++;
+            }
+            if (i < 6) //Check course limit
+            {
+                s.Courses.Add(nc);
+            }
+        }
+    }
+
+    internal class Course
+    {
+        public string Status { get; set; }
+    }
+
+    internal class Student
+    {
+        public List<Course> Courses { get; set; }
+    }
+}
---
+++ Challenges/Naming/02. Meaningful Words/Doctor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Naming._02._Meaningful_Words
+{
+    /// <summary>
+    /// 1. Identify all the names that contain meaningless words.
+    /// 2. Use the refactoring tool supported by your IDE to rename these identifiers into something meaningful.
+    /// </summary>
+    class DoctorInfo
+    {
+        private int _doctorId;
+        private ISet<Specialization> _specializations;
+
+        public bool HasSpecializations(ISet<Specialization> specializationSet)
+        {
+            foreach (var specialization in specializationSet)
+            {
+                if (!_specializations.Contains(specialization)) return false;
+            }
+
+            return true;
+        }
+
+        public void AssignSpecialization(Specialization specializationData)
+        {
+            if (_specializations.Contains(specializationData)) throw new TheDoctorAlreadyHasSpecializationException("Doctor: " + _doctorId.ToString() + "Spec: " + specializationData);
+            _specializations.Add(specializationData);
+        }
+
+    }
+
+    internal class TheDoctorAlreadyHasSpecializationException : Exception
+    {
+        public TheDoctorAlreadyHasSpecializationException(string doctorId) : base(doctorId)
+        {
+        }
+    }
+
+    internal class Specialization
+    {
+        private string _nameStr;
+    }
+}
