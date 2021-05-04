f5377d3ba822231400383b96f775d81cf27277bb
Nikola Luburic <luburic91@gmail.com>
2021-05-04 15:44:36 +0200

    refactoring: Enables Naming challenges to be tested.

--- Challenges/Naming/01. Noise Words/Doctor.cs
+++ Challenges/Naming/01. Noise Words/Doctor.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Naming.Noise
 {
     /// <summary>
-    /// ** Do not modify the namespace name or the Run class name and Main method header. **
+    /// ** Do not modify the namespace name or the Run class method headers. **
     /// 1. Identify all the names that contain meaningless words.
     /// 2. Remove or rename these identifiers into something meaningful.
+    /// 3. Ensure there are no syntax errors and that the Run class is aware of any renaming of the other classes and their members.
     /// </summary>
-    class DoctorInfo
+    public class DoctorInfo
     {
-        private int _doctorId;
-        private ISet<Specialization> _specializations;
+        public int DoctorId { get; }
+        public ISet<Specialization> Specializations { get; } = new HashSet<Specialization>();
 
-        public bool HasSpecializations(ISet<Specialization> specializationSet)
+        public bool HasSpecializations(List<Specialization> specializationSet)
         {
             foreach (var specialization in specializationSet)
             {
-                if (!_specializations.Contains(specialization)) return false;
+                if (!Specializations.Contains(specialization)) return false;
             }
 
             return true;
@@ -25,21 +27,67 @@ namespace Naming.Noise
 
         public void AssignSpecialization(Specialization specializationData)
         {
-            if (_specializations.Contains(specializationData)) throw new TheDoctorAlreadyHasSpecializationException("Doctor: " + _doctorId.ToString() + "Spec: " + specializationData);
-            _specializations.Add(specializationData);
+            if (Specializations.Contains(specializationData)) throw new TheDoctorAlreadyHasSpecializationException("Doctor: " + DoctorId.ToString() + "Spec: " + specializationData);
+            Specializations.Add(specializationData);
         }
 
     }
 
-    internal class TheDoctorAlreadyHasSpecializationException : Exception
+    public class TheDoctorAlreadyHasSpecializationException : Exception
     {
         public TheDoctorAlreadyHasSpecializationException(string doctorId) : base(doctorId)
         {
         }
     }
 
-    internal class Specialization
+    public class Specialization
     {
-        private string _nameStr;
+        private readonly string _nameStr;
+
+        public Specialization(string name)
+        {
+            _nameStr = name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Specialization other)) return false;
+            return other._nameStr.Equals(_nameStr);
+        }
+
+        public override int GetHashCode()
+        {
+            return _nameStr.GetHashCode();
+        }
     }
+
+    #region Run
+    public class Run
+    {
+        private readonly DoctorInfo _doctor;
+
+        public Run()
+        {
+            _doctor = new DoctorInfo();
+            _doctor.AssignSpecialization(new Specialization("Test 1"));
+            _doctor.AssignSpecialization(new Specialization("Test 2"));
+            _doctor.AssignSpecialization(new Specialization("Test 3"));
+        }
+
+        public void AddSpec()
+        {
+            _doctor.AssignSpecialization(new Specialization("Test 1"));
+        }
+
+        public List<Specialization> GetSpec()
+        {
+            return _doctor.Specializations.ToList();
+        }
+
+        public bool HasSpec(List<Specialization> all)
+        {
+            return _doctor.HasSpecializations(all);
+        }
+    }
+    #endregion
 }
--- Challenges/Naming/02. Meaningful Words/Course.cs
+++ Challenges/Naming/02. Meaningful Words/Course.cs
@@ -5,10 +5,10 @@ namespace Naming.Meaning
     /// <summary>
     /// ** Do not modify the namespace name or the Run class name and Main method header. **
     /// 1. Identify and rename all the identifiers with poor names.
-	/// 2. Identify any literals in the code and find a way to make their purpose clear.
-	/// 3. Identify at least one piece of logic whose intent is not clear and extract it into a method with a meaningful name.
+    /// 2. Identify any literals in the code and find a way to make their purpose clear.
+    /// 3. Identify at least one piece of logic whose intent is not clear and extract it into a method with a meaningful name.
     /// </summary>
-    class CourseService
+    public class CourseService
     {
         public void Add(Course nc, Student s)
         {
@@ -25,13 +25,40 @@ namespace Naming.Meaning
         }
     }
 
-    internal class Course
+    public class Course
     {
         public string Status { get; set; }
     }
 
-    internal class Student
+    public class Student
     {
-        public List<Course> Courses { get; set; }
+        public List<Course> Courses { get; set; } = new List<Course>();
     }
+
+    #region Run
+    public class Run
+    {
+        private readonly Student _student = new Student();
+        private readonly CourseService _service = new CourseService();
+
+        public Run()
+        {
+            _service.Add(new Course { Status = "enrolled" }, _student);
+            _service.Add(new Course { Status = "current" }, _student);
+            _service.Add(new Course { Status = "enrolled" }, _student);
+            _service.Add(new Course { Status = "current" }, _student);
+            _service.Add(new Course { Status = "current" }, _student);
+        }
+
+        public void AddCourse()
+        {
+            _service.Add(new Course { Status = "enrolled" }, _student);
+        }
+
+        public int Count()
+        {
+            return _student.Courses.Count;
+        }
+    }
+    #endregion
 }
