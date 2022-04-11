56324f5bc08b2548e252b8d0a32cee052dd1cf29
Nikola Luburic <luburic91@gmail.com>
2022-04-11 07:41:22 +0200

    chore: Minor exercise comment adjustment.

--- Challenges/Naming/02. Domain Words/Course.cs
+++ Challenges/Naming/02. Domain Words/Course.cs
@@ -4,7 +4,7 @@ namespace Naming.Domain
 {
     /// <summary>
     /// ID izazova je dostupan na web prikazu.
-    /// ** Nemoj da modifikuješ naziv namespace-a, Run klase i zaglavlja metoda ove klase. **
+    /// ** Nemoj da modifikuješ naziv ove klase. **
     /// 1. Identifikuj i preimenuj sve nejasne nazive tako da koriste domensku terminologiju.
     /// 2. Instrukcije "c.Status.Equals("enrolled") || c.Status.Equals("current")" možemo izdvojiti u zasebnu metodu
     ///     čiji naziv objašnjava značenje ove logike. Napravi ovakvu funkciju i dodeli joj domenski značajan naziv.
@@ -36,31 +36,4 @@ namespace Naming.Domain
     {
         public List<Course> Courses { get; set; } = new List<Course>();
     }
-
-    #region Run
-    public class Run
-    {
-        private readonly Student _student = new Student();
-        private readonly CourseService _service = new CourseService();
-
-        public Run()
-        {
-            _service.Add(new Course { Status = "enrolled" }, _student);
-            _service.Add(new Course { Status = "current" }, _student);
-            _service.Add(new Course { Status = "enrolled" }, _student);
-            _service.Add(new Course { Status = "current" }, _student);
-            _service.Add(new Course { Status = "current" }, _student);
-        }
-
-        public void AddCourse()
-        {
-            _service.Add(new Course { Status = "enrolled" }, _student);
-        }
-
-        public int Count()
-        {
-            return _student.Courses.Count;
-        }
-    }
-    #endregion
 }
