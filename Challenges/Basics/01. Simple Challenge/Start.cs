9251f9a21a5f39d32c9290d5614648bf7855f9bb
Nikola Luburic <luburic91@gmail.com>
2022-04-11 07:30:18 +0200

    chore: Slightly alters comment.

--- Challenges/Basics/01. Simple Challenge/Start.cs
+++ Challenges/Basics/01. Simple Challenge/Start.cs
@@ -8,7 +8,7 @@ namespace Basics.Simple
     /// ID izazova je dostupan na web prikazu.
     /// 1. Izvrši Submit komandu nad Start.cs datotekom.
     /// 2. Ispitaj panel koji se otvara u VS Code-u, otvori hint i isprati šta ti govori.
-    /// 3. Preimenuj Challenge atribut spram hinta (F2 je podrazumevana prečica).
+    /// 3. Preimenuj Challenge atribut spram hinta (F2 je podrazumevana prečica) i sačuvaj izmenu.
     /// 4. Izvrši Submit komandu nad Start.cs datotekom.
     /// 5. Analiziraj panel i otvori rešenje.
     /// 6. Na web prikazu stisni taster "Osveži tačnost submisije".
